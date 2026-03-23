11cdf400e546a831281ddd3916263b48fffec87d
TRISKANV <borrarcuenta0800@gmail.com>
2026-03-23 20:04:31 -0300

    Create games.js

---
+++ src/constants/games.js
@@ -0,0 +1,22 @@
+// src/constants/games.js
+
+export const GAME_ASSETS = {
+    cs16: {
+        name: 'Counter-Strike 1.6',
+        icon: require('../../assets/cs16.png'),
+    },
+    csgo: {
+        name: 'CS:GO',
+        icon: require('../../assets/csgo.png'),
+    },
+    hl: {
+        name: 'Half-Life',
+        icon: require('../../assets/hl.png'),
+    },
+    tf2: {
+        name: 'Team Fortress 2',
+        icon: require('../../assets/tf2.png'),
+    },
+    // Si tienes un icono por defecto para cuando algo falle
+    default: require('../../assets/default.png'), 
+};
